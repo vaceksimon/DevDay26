a31a8199f26879df0e3a0bf686afc559e318147d
Simon Vacek <simonvacky@email.cz>
2026-03-02 23:54:37 +0100

    Scenario 2: init with adminclient and master realm
    Signed-off-by: Simon Vacek <simonvacky@email.cz>

---
+++ src/test/java/org/keycloak/devday/scenarios/DevDay2AdminSignatureAlgorithmTest.java
@@ -0,0 +1,23 @@
+package org.keycloak.devday.scenarios;
+
+import org.junit.jupiter.api.Test;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.testframework.annotations.InjectAdminClient;
+import org.keycloak.testframework.annotations.InjectRealm;
+import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
+import org.keycloak.testframework.realm.ManagedRealm;
+
+@KeycloakIntegrationTest
+public class DevDay2AdminSignatureAlgorithmTest {
+
+    @InjectAdminClient
+    Keycloak admin;
+
+    @InjectRealm(attachTo = "master")
+    ManagedRealm masterRealm;
+
+    @Test
+    public void changeRealmTokenAlgorithm() {
+        // TODO
+    }
+}
