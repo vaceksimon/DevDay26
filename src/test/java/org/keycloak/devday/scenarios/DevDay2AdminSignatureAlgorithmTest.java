ab72b7773a0ce93c7ff011ace93a74015f586236
Simon Vacek <simonvacky@email.cz>
2026-03-02 23:59:28 +0100

    Scenario 2 FINISHED: test done
    Signed-off-by: Simon Vacek <simonvacky@email.cz>

--- src/test/java/org/keycloak/devday/scenarios/DevDay2AdminSignatureAlgorithmTest.java
+++ src/test/java/org/keycloak/devday/scenarios/DevDay2AdminSignatureAlgorithmTest.java
@@ -1,12 +1,17 @@
 package org.keycloak.devday.scenarios;
 
 import org.junit.jupiter.api.Test;
+import org.keycloak.TokenVerifier;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.crypto.Algorithm;
+import org.keycloak.representations.AccessToken;
+import org.keycloak.representations.AccessTokenResponse;
 import org.keycloak.testframework.annotations.InjectAdminClient;
 import org.keycloak.testframework.annotations.InjectRealm;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
 import org.keycloak.testframework.realm.ManagedRealm;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @KeycloakIntegrationTest
 public class DevDay2AdminSignatureAlgorithmTest {
@@ -18,9 +23,13 @@ public class DevDay2AdminSignatureAlgorithmTest {
     ManagedRealm masterRealm;
 
     @Test
-    public void changeRealmTokenAlgorithm() {
+    public void changeRealmTokenAlgorithm() throws Exception {
         masterRealm.updateWithCleanup(r -> r.defaultSignatureAlgorithm(Algorithm.ES256));
 
-        // TODO
+        admin.tokenManager().invalidate(admin.tokenManager().getAccessTokenString());
+
+        AccessTokenResponse accessToken = admin.tokenManager().getAccessToken();
+        TokenVerifier<AccessToken> verifier = TokenVerifier.create(accessToken.getToken(), AccessToken.class);
+        assertEquals(Algorithm.ES256, verifier.getHeader().getAlgorithm().name());
     }
 }
