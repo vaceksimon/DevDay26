cedac93f7e84d679847545ab8bd62693cb7f1763
Simon Vacek <simonvacky@email.cz>
2026-03-03 00:04:48 +0100

    Scenario 3: server config
    Signed-off-by: Simon Vacek <simonvacky@email.cz>

--- src/test/java/org/keycloak/devday/scenarios/DevDay3ImpersonationDisabledTest.java
+++ src/test/java/org/keycloak/devday/scenarios/DevDay3ImpersonationDisabledTest.java
@@ -1,13 +1,16 @@
 package org.keycloak.devday.scenarios;
 
 import org.junit.jupiter.api.Test;
+import org.keycloak.common.Profile;
 import org.keycloak.testframework.annotations.InjectRealm;
 import org.keycloak.testframework.annotations.InjectUser;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
 import org.keycloak.testframework.realm.ManagedRealm;
 import org.keycloak.testframework.realm.ManagedUser;
+import org.keycloak.testframework.server.KeycloakServerConfig;
+import org.keycloak.testframework.server.KeycloakServerConfigBuilder;
 
-@KeycloakIntegrationTest
+@KeycloakIntegrationTest(config = DevDay3ImpersonationDisabledTest.ServerConfig.class)
 public class DevDay3ImpersonationDisabledTest {
 
     @InjectRealm
@@ -20,4 +23,14 @@ public class DevDay3ImpersonationDisabledTest {
     public void testImpersonationDisabled() {
         // TODO
     }
+
+    static class ServerConfig implements KeycloakServerConfig {
+
+        @Override
+        public KeycloakServerConfigBuilder configure(KeycloakServerConfigBuilder config) {
+            return config.featuresDisabled(Profile.Feature.IMPERSONATION);
+        }
+
+    }
+
 }
