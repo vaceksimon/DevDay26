904daaa854775799737f34bf20e1bfe42c3c42f2
Simon Vacek <simonvacky@email.cz>
2026-03-03 00:11:05 +0100

    Scenario 4: user config
    Signed-off-by: Simon Vacek <simonvacky@email.cz>

--- src/test/java/org/keycloak/devday/scenarios/DevDay4ConsentsTest.java
+++ src/test/java/org/keycloak/devday/scenarios/DevDay4ConsentsTest.java
@@ -6,6 +6,8 @@ import org.keycloak.testframework.annotations.InjectUser;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
 import org.keycloak.testframework.realm.ManagedRealm;
 import org.keycloak.testframework.realm.ManagedUser;
+import org.keycloak.testframework.realm.UserConfig;
+import org.keycloak.testframework.realm.UserConfigBuilder;
 
 @KeycloakIntegrationTest
 public class DevDay4ConsentsTest {
@@ -13,11 +15,23 @@ public class DevDay4ConsentsTest {
     @InjectRealm
     ManagedRealm realm;
 
-    @InjectUser
+    @InjectUser(config = ProviderRealmUserConf.class)
     ManagedUser user;
 
     @Test
     public void testConsentCancel() {
         // TODO
+    }
+
+    private static class ProviderRealmUserConf implements UserConfig {
+
+        @Override
+        public UserConfigBuilder configure(UserConfigBuilder builder) {
+            return builder.name("Provider", "User")
+                    .username("provider")
+                    .password("password")
+                    .email("provider@local")
+                    .emailVerified(true);
+        }
     }
 }
