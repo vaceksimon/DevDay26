20ce96419e149d3290079cd1bb391f03f54850fb
Simon Vacek <simonvacky@email.cz>
2026-03-02 23:45:01 +0100

    Scenario 1: mailserver & oauthclient
    Signed-off-by: Simon Vacek <simonvacky@email.cz>

--- src/test/java/org/keycloak/devday/scenarios/DevDay1EmailEventListenerTest.java
+++ src/test/java/org/keycloak/devday/scenarios/DevDay1EmailEventListenerTest.java
@@ -5,6 +5,10 @@ import org.keycloak.events.email.EmailEventListenerProviderFactory;
 import org.keycloak.testframework.annotations.InjectRealm;
 import org.keycloak.testframework.annotations.InjectUser;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
+import org.keycloak.testframework.mail.MailServer;
+import org.keycloak.testframework.mail.annotations.InjectMailServer;
+import org.keycloak.testframework.oauth.OAuthClient;
+import org.keycloak.testframework.oauth.annotations.InjectOAuthClient;
 import org.keycloak.testframework.realm.ManagedRealm;
 import org.keycloak.testframework.realm.ManagedUser;
 import org.keycloak.testframework.realm.RealmConfig;
@@ -20,6 +24,13 @@ public class DevDay1EmailEventListenerTest {
 
     @InjectUser(config = UserWithEmail.class)
     ManagedUser user;
+
+    @InjectMailServer
+    MailServer mail;
+
+    @InjectOAuthClient
+    OAuthClient oAuthClient;
+
 
     @Test
     public void testFailedLoginEmailEvent() {
