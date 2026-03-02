ebf7f6de1ec9d830994969b2a2d7075db31fe801
Simon Vacek <simonvacky@email.cz>
2026-03-03 00:34:43 +0100

    Extension FINISHED: test done
    Signed-off-by: Simon Vacek <simonvacky@email.cz>

--- src/test/java/org/keycloak/devday/extension/DevDayProviderTest.java
+++ src/test/java/org/keycloak/devday/extension/DevDayProviderTest.java
@@ -1,11 +1,18 @@
 package org.keycloak.devday.extension;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.keycloak.common.util.KeycloakUriBuilder;
+import org.keycloak.http.simple.SimpleHttp;
 import org.keycloak.testframework.annotations.InjectRealm;
+import org.keycloak.testframework.annotations.InjectSimpleHttp;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
 import org.keycloak.testframework.realm.ManagedRealm;
 import org.keycloak.testframework.server.KeycloakServerConfig;
 import org.keycloak.testframework.server.KeycloakServerConfigBuilder;
+
+import java.io.IOException;
+import java.net.URL;
 
 @KeycloakIntegrationTest(config = DevDayProviderTest.ServerConfig.class)
 public class DevDayProviderTest {
@@ -13,10 +20,18 @@ public class DevDayProviderTest {
     @InjectRealm
     ManagedRealm realm;
 
+    @InjectSimpleHttp
+    SimpleHttp simpleHttp;
+
     @Test
-    public void providerTest() {
-        // TODO
+    public void providerTest() throws IOException {
+        URL url = KeycloakUriBuilder.fromUri(realm.getBaseUrl()).path("/devday/hello").build().toURL();
+
+        String response = simpleHttp.doGet(url.toString()).header("Accept", "text/plain").asString();
+
+        Assertions.assertEquals("Hello Keycloak DevDay 26!", response);
     }
+
 
     static class ServerConfig implements KeycloakServerConfig {
 
